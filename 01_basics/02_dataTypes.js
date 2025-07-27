c30ff84c065e0c3a55dc1008debfef42739d34c8
Anjal Sotang <anjalsotang26@gmail.com>
2025-07-27 23:32:51 +0545

    Lets get Started

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+// Datatypes and ECMA standards
+
+"use strict"; //treat all code as a newer/strict mode
+
+// alert("Hello World!"); //We are using nodejs, not browser
+
+//tc39 for ECMAScript standards
+// https://tc39.es/ecma262/ 
+
---
+++ 01_basics/03_datatypesConversion.js
@@ -0,0 +1,47 @@
+let score = "33abc"
+
+// console.log(typeof score) // number
+// console.log(typeof(score)) // number
+
+// let valyeInNumber = Number(score)
+// console.log(typeof valyeInNumber) // number
+// console.log(valyeInNumber) // NaN
+
+//"33" => 33
+//"33abc" => NaN
+//"true" => 1
+//"false" => 0
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn) // true
+
+// "" => false
+// " " => true
+// "Anjal" => true
+
+
+// ****Operations****
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue) // -3
+
+
+let str1= "Anjal"
+let str2 = "Kumar"
+let fullName = str1 + " " + str2
+// console.log(fullName) // Anjal Kumar
+
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32
+console.log(1 + 2 + "2" + 3) // 323
+
+let gameCounter = 100
+++gameCounter; //Prefix increment
+gameCounter++; // Postfix increment
+console.log(gameCounter) // 102
